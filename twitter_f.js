24481b0fcc93e70ebf102fb72a1d913c49535daa
Heipiao <lsl8315@gmail.com>
2017-04-26 18:08:36 +0100

    some message for this project.

---
+++ twitter_f.js
@@ -0,0 +1,30 @@
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         'FSu9mlFZrbfnggLqZIwu5MXLy',
+  consumer_secret:      'xXBEPeCbnw5L7TgB65CuWD5aliYFlS2b95xzjMLFnIFCPGveuO',
+  access_token:         '784846164868861952-KAoZ4M7Y45KEhYvip6O56hm7RAbKbXQ',
+  access_token_secret:  'OEGm8yITHPc4DHQEhMluzYvlYewEtHK4chY3h4F7ORIoI',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+var query = 'I from:ManUtd'
+var twitterList = []
+var twitterNode = {}
+
+T.get('search/tweets', { q: query, count: 20 }, function(err, data, response) { 
+  if(err){
+    console.log(err)
+  }
+  for( var i =0 ;i< data.statuses.length;i++)
+  {
+    twitterNode = {
+      id: data.statuses[i].id,
+      time: data.statuses[i].created_at,
+      text: data.statuses[i].text
+    }
+    twitterList.push(twitterNode)
+  }
+})
+
+console.log(twitterList)
